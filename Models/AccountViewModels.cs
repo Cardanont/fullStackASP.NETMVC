19c4539f56f836908f566d44ab60366fcef45a9d
Daniel Ontiveros <cardanont@gmail.com>
2019-08-09 23:19:45 -0500

    Add Phone to register

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -69,6 +69,9 @@ namespace FullStackMVC5.Models
         public string DrivingLicense { get; set; }
 
         [Required]
+        public string Phone { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }

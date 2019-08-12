73421904aff041acfcdad5e5bf57ef658f808ab1
Daniel Ontiveros <cardanont@gmail.com>
2019-08-12 16:05:58 -0500

    Add Rentals API models and all the stuff

---
+++ Controllers/Api/NewRentalsDtoController.cs
@@ -0,0 +1,19 @@
+﻿using FullStackMVC5.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FullStackMVC5.Controllers.Api
+{
+    public class NewRentalsDtoController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -26,24 +26,4 @@ namespace FullStackMVC5.Models
             return userIdentity;
         }
     }
-
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Movie> Movies { get; set; }
-        public DbSet<MembershipType> MembershipTypes { get; set; }
-
-        public DbSet<MovieGenre> MovieGenres { get; set; }
-
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
 }
